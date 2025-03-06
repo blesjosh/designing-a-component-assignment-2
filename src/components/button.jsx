4ffea7b205774733a01190ccc45917a08e64efce
Blessing Joshua <blessingjoshuaoffl@gmail.com>
2025-03-06 10:24:26 +0530

    done

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,13 @@
-// write button card here
+// write button card here// write button card here
+import React from 'react'
+
+const Button = () => {
+  return (
+    <div>
+
+      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>View Product</button>
+    </div>
+  )
+}
+
+export default Button
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,20 @@
 // write product card here
+import React from 'react'
+import Button from './button.jsx';
+const productcard = () => {
+  const image ="https://shorturl.at/zPGhL";
+  const name = "Product Name";
+  const price = "$99.99";
+  return (
+    <>
+      <div>
+        <img src={image} alt={name} />
+        <h2>{name}</h2>
+        <p>{price}</p>
+        <Button />
+      </div>
+    </>
+  );
+}
+
+export default productcard
